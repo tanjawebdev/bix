0a46d2582a35f97402664a56b1f1d9f7044cdc0b
Tanja Kobler <kobler.tanja@gmail.com>
2025-05-22 16:57:31 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -77,14 +77,13 @@ export default function Page() {
             console.log("test");
             const image = canvas.toDataURL('image/png')
 
-            fetch('http://172.20.10.8:3001/update', {
-                //172.20.10.8
-                //192.168.50.213:3001
-                method: 'POST',
+            fetch("http://172.20.10.8:3001/update", {
+                method: "POST",
                 mode: "no-cors",
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ image }),
-            }).catch((err) => console.error('NDI update failed', err))
+                body: JSON.stringify({
+                    image: canvas.toDataURL("image/png"),
+                }),
+            }).catch(e => console.error("NDI update failed", e))
         }, 100)
 
         return () => clearInterval(interval)
